db081ee3e791556a01809304b980c7813805ffb7
Eriq Abonie <e.abonie@gmail.com>
2021-10-24 15:11:42 +0100

    getting older, getting unhealthier complete. but need to refactor

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -28,4 +28,34 @@ describe('constructor', () => {
     expect(pet.age).toEqual(1);
   });
 
+  it('has a initial hunger of 0', () => {
+    const pet = new Pet('Fido');
+
+    expect(pet.hunger).toEqual(0);
+  });
+
+    // need to figure out how to save and call the inital so I can decrease by subtracting when the grow up fuction is called
+  it('increments the hunger by 5', () => {
+    const pet = new Pet('Fido');
+
+    pet.growUp();
+
+    expect(pet.hunger).toEqual(5);
+  });
+
+  it('has a initial fitness of 10', () => {
+    const pet = new Pet('Fido');
+
+    expect(pet.fitness).toEqual(10);
+  });
+
+     // need to figure out how to save and call the inital so I can decrease by subtracting when the grow up fuction is called
+  it('decrease the fitness by 3', () => {
+    const pet = new Pet('Fido');
+
+    pet.growUp();
+
+    expect(pet.fitness).toEqual(7);
+  });
+
 });
--- scr/pet.js
+++ scr/pet.js
@@ -2,11 +2,15 @@ function Pet(petName) {
 
  this.petName = petName; 
  this.age = 0;
+ this.hunger = 0;
+ this.fitness = 10;
 
 }
 
 Pet.prototype.growUp = function() {
     this.age += 1;
+    this.hunger += 5;
+    this.fitness -= 3;
 }
 
 
